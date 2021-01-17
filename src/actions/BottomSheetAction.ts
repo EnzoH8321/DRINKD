353c3f06961c75bffbff22196955cc6919b11399
Enzo Herrera <enzoherrera123@gmail.com>
2021-01-16 21:23:24 -0800

    removed unneeded redux files

--- src/actions/BottomSheetAction.ts
+++
@@ -1,7 +0,0 @@
-export const SET_BOTTOM_STYLE = "SET BOTTOM STYLE";
-// export const GET_DATA_FAILURE = 'GET DATA FAILURE'
-
-export const BottomSheetAction = (action) => ({
-  type: SET_BOTTOM_STYLE,
-  payload: action,
-});
