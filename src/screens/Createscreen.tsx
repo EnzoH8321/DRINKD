ce4c858e2bd13dda3c9621c902bff639c590b2b6
Enzo Herrera <enzoherrera123@gmail.com>
2021-01-23 10:52:14 -0800

    included the create party func

--- src/screens/Createscreen.tsx
+++ src/screens/Createscreen.tsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Headline, Button } from "react-native-paper";
 import styles from "../styles/constant";
 
-function createPartyCode() {
-  const randomNumber = Math.floor(
-    Math.pow(10, 8 - 1) + Math.random() * 9 * Math.pow(10, 8 - 1)
-  ).toString();
-
-  return randomNumber;
-}
+//firebase
+import firebase from "../utils/firebase";
 
 const CreateScreen: React.FC = () => {
+  function createPartyCode() {
+    const randomNumber = Math.floor(
+      Math.pow(10, 8 - 1) + Math.random() * 9 * Math.pow(10, 8 - 1)
+    ).toString();
+
+    firebase.database().ref("parties/").set({
+      userId: randomNumber,
+    });
+
+    setPartyCode(randomNumber);
+  }
+
+  const [partyCode, setPartyCode] = useState("");
+
   return (
     <View style={[styles.container, override.view]}>
       <Button mode="contained" style={styles.button} onPress={createPartyCode}>
         Create Party
       </Button>
-      <Headline style={override.headline}>Your party code is xxx</Headline>
+      <Headline style={override.headline}>
+        Your Party Code is {partyCode}
+      </Headline>
     </View>
   );
 };
