0296fa0ba2dfd1e21bf48bd0d1b1dbd0615a6982
Enzo Herrera <enzoherrera123@gmail.com>
2021-01-22 21:05:44 -0800

    added firebase

---
+++ firebase.ts
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOxlOHr3T3T7EY-g2YQualK6QC8ybwMkE",
+  authDomain: "drinkd-dev.firebaseapp.com",
+  databaseURL: "https://drinkd-dev-default-rtdb.firebaseio.com",
+  projectId: "drinkd-dev",
+  storageBucket: "drinkd-dev.appspot.com",
+  messagingSenderId: "811366010805",
+  appId: "1:811366010805:web:e889f8bbf3e51cb71e53fc",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
