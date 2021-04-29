95e9cf58c055ccc6c707c964dba7b0f3eb5c201b
Enzo Herrera <enzoherrera123@gmail.com>
2021-04-28 22:07:21 -0700

    Fixed issue were public facing API key was being shown

--- src/api/YelpApi.ts
+++ src/api/YelpApi.ts
@@ -9,8 +9,7 @@ export default async function fetchBusiness(
     const data = await axios(url, {
       method: "GET",
       headers: {
-        Authorization:
-          "BEARER nX9W-jXWsXSB_gW3t2Y89iwQ-M7SR9-HVBHDAqf1Zy0fo8LTs3Q1VbIVpdeyFu7PehJlkLDULQulnJ3l6q6loIET5JHmcs9i3tJqYEO02f39qKgSCi4DAEVIlgPPX3Yx",
+        Authorization: `${process.env.YELP_API_KEY}`,
       },
     });
 
