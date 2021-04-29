7167faf5252ee0e1dd10eb75ff2fafa0144be826
Enzo Herrera <enzoherrera123@gmail.com>
2021-04-28 22:49:12 -0700

    Fixed issue were public facing API key was being shown

--- src/api/YelpApi.ts
+++ src/api/YelpApi.ts
@@ -5,11 +5,13 @@ import { ApiSearch } from "../types/types";
 export default async function fetchBusiness(
   url: string
 ): Promise<[ApiSearch] | undefined> {
+  console.log("process", process.env.REACT_APP_YELP_API_KEY);
   try {
     const data = await axios(url, {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+        Authorization:
+          "BEARER nX9W-jXWsXSB_gW3t2Y89iwQ-M7SR9-HVBHDAqf1Zy0fo8LTs3Q1VbIVpdeyFu7PehJlkLDULQulnJ3l6q6loIET5JHmcs9i3tJqYEO02f39qKgSCi4DAEVIlgPPX3Yx",
       },
     });
 
@@ -18,6 +20,6 @@ export default async function fetchBusiness(
     //Returns an array of businesses
     return businessData;
   } catch (error) {
-    console.log("the error is" + error);
+    console.log("the error is " + error);
   }
 }
